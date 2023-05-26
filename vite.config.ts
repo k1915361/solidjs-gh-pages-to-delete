4b7e085130b698cd59639dd8e0249caf709ca2bd
k1915361 <67165821+k1915361@users.noreply.github.com>
2023-05-26 16:04:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/solidjs-github-pages/",
+    base: "/solidjs-gh-pages/",
     plugins: [solid({ adapter: staticAdapter() })]
 });
